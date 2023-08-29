183a787291f1afde5b6a915b80d25c358190acf6
pedrohenrique <pedrohenriquegomesmartins15@gmail.com>
2023-08-29 11:03:25 -0300

    adicionando processos de leitura e edição

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -15,7 +15,7 @@ exports.create = (req, res) => {
         description: req.body.description,
         quantity: req.body.quantity,
         is_flammble: req.body.is_flammble ? req.body.is_flammble: false
-    }
+    };
 
     Item.create(item)
     .then(data => {
@@ -24,10 +24,10 @@ exports.create = (req, res) => {
     .catch(err => {
         res.status(500).send({
             message:
-            err.message || "Ocorreu um erro ao criar o item."
-        })
-    })
-    };
+            err.message || "Algum erro aconteceu ao tentar criar o item, Ocorreu um erro ao criar o item desejado, Por favor corrigir o seu erro para criar o seu item desejado."
+        });
+    });
+};
 
 exports.findAll = (req, res) => {
     const name = req.body.name;
@@ -40,22 +40,77 @@ exports.findAll = (req, res) => {
     .catch(err => {
         res.status(500).send({
             message:
-            err.message || "Ocorreu um erro ao listar os itens."
-        })
-        
-    })
+            err.message || "Algum erro aconteceu ao tentar pesquisar pelos itens, Ocorreu um erro ao pesquisar os itens desejados, Por favor corrigir o seu erro para entrar a sua pesquisar desejada."
+        });  
+    });
 };
 
-    exports.findOne = (req, res) => {
+exports.findOne = (req, res) => {
+    const id = res.params.id;
 
+    Item.findByPk(id)
+    .then(data => {
+        if (data) {
+        res.send(data);
+        } else { 
+        res.status(404).send({
+             message: `Não foi possível encontrar um item com o id=${id}.`
+        });
+        }
+    })
+    .catch(err => {
+        res.status(508),send({
+        message: "Ocorreu um erro ao tentar encontrar um item com o id=" + id
+        });
+    });
 };
 
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+    Item.update(req.body, {
+        where: { id: id }
+    })
+    .then(num => {
+      if (num == 1) {
+          res.send({
+              message: "O item foi atualizado de maneira bem sucedida."
+          });
+        } else {
+            res.send({
+                message: `Não foi possível atualizar o item com o id=${id}.`
+            });
+           }
+        })
+    .catch(err => {
+        res.status(500).send({
+            message: "Ocorreu um erro ao tentar atualizar um item com o id=" + id
+        });
+    });
 };
 
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Item.destroy({
+        where: {id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res,send({
+                message: "O item foi apagado com sucesso!"
+            });
+        } else {
+            res.send({
+                message: `Não foi possível apagar o item com o id=${id}.`
+            });
+           }
+        })
+    .catch(err => {
+        res.status().send({
+            message: "Ocorreu um erro ao tentar apagar o item com o id=" + id
+        });
+    });
 };
 
 exports.deleteAll = (req, res) => {
